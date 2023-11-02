c6feeaefde28500bc21c292a095ad16533a35422
hendr-22 <147894144+hendr-22@users.noreply.github.com>
2023-11-02 19:06:37 +0700

    Add files via upload

---
+++ beforeinstallscripts/2-install-dwm-dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the script is running with root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root or with sudo."
+  exit 1
+fi
+
+# Update the package database and upgrade installed packages
+pacman -Syu --noconfirm
+
+# Install htop, neofetch, nnn, and xorg-server with noconfirm to avoid interactive prompts
+pacman -S libx11 libxft libxinerama git lxsession --noconfirm
+
+# Check if the installation was successful
+if [ $? -eq 0 ]; then
+  echo "Installation completed successfully."
+else
+  echo "Installation encountered an error."
+fi
